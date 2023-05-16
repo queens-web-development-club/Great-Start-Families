6fe87cd0a21ec4c202b2a1cf489270562bfe3474
Daniel Dousek <danieldousek@gmail.com>
2023-05-16 12:05:24 -0400

    Homepage fully responsive

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,18 +5,23 @@ function HomePage() {
   return (
     <div className=' md:text-left relative text-white'>
       <img src={responsiveImage} alt='A responsive image' className='w-full h-auto' />
-      <div class=' absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:top-1/4 md:left-20 md:transform-none'>
-        <div class='bg-opacity-60 bg-black rounded-lg p-4 md:p-6 shadow-md '>
-          <h2 className='opacity-100 text-white text-3xl md:text-5xl font-semibold mb-4'>
-            Programs for <br /> families in need.
+
+      <div class=' absolute  w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 sm:w-auto md:top-10 md:left-20 md:transform-none lg:top-20 xl:top-36'>
+        <div class='bg-opacity-60 bg-black sm:rounded-3xl p-4 md:p-6 shadow-md '>
+          <h2 className='opacity-100 text-white text-2xl sm:text-4xl md:text-7xl  font-semibold mb-2 sm:mb-4 xl:text-8xl'>
+            Programs for <br className='hidden sm:block' /> families in need.
           </h2>
-          <p className='opacity-100 mb-4 text-sm md:text-base'>
-            Monlthy events organized by the Kingston community. <br /> Anyone of all ages can drop by!
+          <p className=' opacity-100 mb-4 text-md  md:text-base xl:text-3xl'>
+            Monlthy events organized by the Kingston community. <br className='hidden sm:block' /> Anyone of all ages can drop by!
           </p>
 
-          <div className='flex justify-center space-x-2 md:justify-start md:space-x-4'>
-            <button className='bg-purple-600 text-white px-3 md:px-4 py-1 md:py-2 rounded text-sm md:text-base'>Calender</button>
-            <button className='bg-purple-600 text-white px-3 md:px-4 py-1 md:py-2 rounded text-sm md:text-base'>Resources</button>
+          <div className='flex justify-center space-x-2 md:justify-start md:space-x-4 '>
+            <button className='bg-purple-600 text-white px-3 md:px-6 py-2 md:py-3 rounded-full  md:text-2xl xl:px-10 xl:py-5  xl:text-4xl'>
+              Calender
+            </button>
+            <button className='bg-purple-600 text-white px-3 md:px-6 py-2 md:py-3 rounded-full  md:text-2xl xl:px-10 xl:py-5 xl:text-4xl'>
+              Resources
+            </button>
           </div>
         </div>
       </div>
