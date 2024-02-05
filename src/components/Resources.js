7a2bc1fc3a82c229f432329a892d53f873b6b5d5
Faiyaz Kazi <faiyazk1012@gmail.com>
2024-02-05 13:37:20 -0500

    fixed resources download button

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,15 +1,22 @@
 import React from "react";
-import responsiveImage1 from "../assets//resourses/Nutrition.png";
-import responsiveImage2 from "../assets//resourses/YOUTH.png";
-import responsiveImage3 from "../assets//resourses/baby_care.png";
+import responsiveImage1 from "../assets/resourses/Nutrition.png";
+import responsiveImage2 from "../assets/resourses/YOUTH.png";
+import responsiveImage3 from "../assets/resourses/baby_care.png";
 
 const columns = [
-  { title: "Story Time", img: responsiveImage1, text: "Some information about this  epic first column." },
-  { title: "Parental Help", img: responsiveImage2, text: "Some information about this second column." },
-  { title: "Title 3", img: responsiveImage3, text: "Some information about this third column." },
+  { title: "Story Time", img: responsiveImage1, text: "Some information about this epic first column.", fileUrl: "../../file1.pdf" },
+  { title: "Parental Help", img: responsiveImage2, text: "Some information about this second column.", fileUrl: "../../file2.pdf" },
+  { title: "Title 3", img: responsiveImage3, text: "Some information about this third column.", fileUrl: "../../file3.pdf" },
 ];
 
 function Resources() {
+  const handleDownload = (fileUrl) => {
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.download = fileUrl.split("/").pop();
+    link.click();
+  };
+
   return (
     <div className='px-6 py-4 bg-white' id='resources'>
       <div className='lg:m-10 '>
@@ -23,7 +30,10 @@ function Resources() {
               <img src={column.img} className='h-auto mb-2 bg-gray-300 border-b-4 border-black rounded-t-3xl' />
               <h2 className='mb-1 ml-2 text-xl font-bold lg:text-3xl xl:ml-8'>{column.title}</h2>
               <p className='mb-2 ml-2 text-gray-700 lg:text-xl xl:ml-8'>{column.text}</p>
-              <button className='px-4 py-2 mb-5 ml-2 transition duration-150 ease-in-out bg-purple-600 rounded-full xl:ml-8 lg:px-10 lg:py-3 lg:text-xl hover:bg-purple-500'>
+              <button
+                onClick={() => handleDownload(column.fileUrl)}
+                className='px-4 py-2 mb-5 ml-2 transition duration-150 ease-in-out bg-purple-600 rounded-full xl:ml-8 lg:px-10 lg:py-3 lg:text-xl hover:bg-purple-500'
+              >
                 Download
               </button>
             </div>
